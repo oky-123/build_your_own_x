103fc1ff7dc1ab1099e82fa1a685d2bebb07f2df
oky-123 <oky123.ia@gmail.com>
2019-05-12 23:25:42 +0900

    Add register toke

--- ProgrammingLanuage/iridium/src/assembler/mod.rs
+++ ProgrammingLanuage/iridium/src/assembler/mod.rs
@@ -3,4 +3,6 @@ use crate::instruction::Opcode;
 #[derive(Debug, PartialEq)]
 pub enum Token {
     Op { code: Opcode },
+    Register { reg_num: u8 },
+    IntegerOperand { value: i32 },
 }
--- ProgrammingLanuage/iridium/src/assembler/opcode_parser.rs
+++ ProgrammingLanuage/iridium/src/assembler/opcode_parser.rs
@@ -1,14 +1,30 @@
+use nom::digit;
 use nom::types::CompleteStr;
 
 named!(opcode_load<CompleteStr, Token>,
-       do_parse!(
-           tag!("load") >> (Token::Op{code: Opcode::LOAD})
-           )
-       );
+   do_parse!(
+       tag!("load") >> (Token::Op{code: Opcode::LOAD})
+   )
+);
+
+named!(register<CompleteStr, Token>,
+    do_parse!(
+        ws!(
+           tag!("$") >>
+            reg_num: digit >>
+            (
+                Token::Register{
+                    reg_num: reg_num.parse::<u8>().unwrap()
+                }
+            )
+        )
+    )
+);
 
 mod tests {
     use super::*;
 
+    #[test]
     fn test_opcode_load() {
         let result = opcode_load(CompleteStr("load"));
         assert_eq!(result.is_ok(), true);
@@ -17,6 +33,16 @@ mod tests {
         assert_eq!(rest, CompleteStr(""));
 
         let result = opcode_load(CompleteStr("aold"));
+        assert_eq!(result.is_ok(), false);
+    }
+
+    #[test]
+    fn test_parse_register() {
+        let result = register(CompleteStr("$0"));
+        assert_eq!(result.is_ok(), true);
+        let result = register(CompleteStr("0"));
+        assert_eq!(result.is_ok(), false);
+        let result = register(CompleteStr("$a"));
         assert_eq!(result.is_ok(), false);
     }
 }
