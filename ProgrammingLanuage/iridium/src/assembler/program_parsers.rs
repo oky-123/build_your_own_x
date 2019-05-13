1227086d2ef8e5e16a422d33a9db4517a739ea3b
oky-123 <oky123.ia@gmail.com>
2019-05-13 10:06:51 +0900

    Add program parser

---
+++ ProgrammingLanuage/iridium/src/assembler/program_parsers.rs
@@ -0,0 +1,33 @@
+use nom::types::CompleteStr;
+
+use crate::assembler::instruction_parsers::{instruction_one, AssemblerInstruction};
+
+#[derive(Debug, PartialEq)]
+pub struct Program {
+    instructions: Vec<AssemblerInstruction>,
+}
+
+named!(pub program<CompleteStr, Program>,
+    do_parse!(
+        instructions: many1!(instruction_one) >>
+        (
+            Program {
+                instructions: instructions
+            }
+        )
+    )
+);
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_program() {
+        let result = program(CompleteStr("load $0 #100\n"));
+        assert_eq!(result.is_ok(), true);
+        let (leftover, p) = result.unwrap();
+        assert_eq!(leftover, CompleteStr(""));
+        assert_eq!(1, p.instructions.len());
+        // TODO: Figure out an ergonomic way to test the AssemblerInstruction returned
+    }
+}
