d77441f916b76f0b89aa9c57856ea679aa19f25a
oky-123 <oky123.ia@gmail.com>
2019-05-13 10:00:33 +0900

    Add parser

--- ProgrammingLanuage/iridium/src/assembler/instruction_parsers.rs
+++ ProgrammingLanuage/iridium/src/assembler/instruction_parsers.rs
@@ -28,3 +28,27 @@ named!(pub instruction_one<CompleteStr, AssemblerInstruction>,
         )
     )
 );
+
+#[cfg(test)]
+mod tests {
+
+    use super::super::*;
+    use super::*;
+
+    #[test]
+    fn test_parse_instruction_form_one() {
+        let result = instruction_one(CompleteStr("load $0 #100\n"));
+        assert_eq!(
+            result,
+            Ok((
+                CompleteStr(""),
+                AssemblerInstruction {
+                    opcode: Token::Op { code: Opcode::LOAD },
+                    operand1: Some(Token::Register { reg_num: 0 }),
+                    operand2: Some(Token::IntegerOperand { value: 100 }),
+                    operand3: None
+                }
+            ))
+        );
+    }
+}
